057ac1c340dc052e0571e8a3341dab33b1dfff06
449lv <114080205+449lv@users.noreply.github.com>
2023-05-01 12:22:09 +0300

    Update settings.js
    Gg

--- settings.js
+++ settings.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs')
 const chalk = require('chalk')
 
@@ -22,9 +21,9 @@ global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
+global.ownernomer = "963981210392" //ur number
+global.premium = ['963981210393'] //ur premium number
+global.botname = 'البوت ليلي' //ur bot name
 global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
